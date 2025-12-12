89ef967f98f7827e1187880962dd46699d9ffdc8
ToPo <topo3.topo3.main@gmail.com>
2025-12-12 19:12:10 +0900

    update

--- step6_aituber_v6_gemma3.py
+++ step6_aituber_v6_gemma3.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     config = TrainingConfig(
         model_name="ToPo-ToPo/rinna-japanese-gpt-neox-3.6b-lora-sft-v1",
         hf_dataset_id="ToPo-ToPo/ai-characters-QA",
-        hf_data_file="dataset-aituber-v6-gamma3-27b.jsonl",
-        peft_name="models/rinna-japanese-gpt-neox-3.6b-lora-aituber-v6-gemma3-27b",
+        hf_data_file="dataset-aituber-v7-gamma3-27b.jsonl",
+        peft_name="models/rinna-japanese-gpt-neox-3.6b-lora-aituber-v7-gemma3-27b",
         epochs=10,
         lora_r=32,
         lora_alpha=64
